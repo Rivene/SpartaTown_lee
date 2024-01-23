b9ec24c98ea673ff349afd166df46f747b47c498
Rivene <abcfgf777@naver.com>
2024-01-23 20:35:03 +0900

    필수 요구사항 구현

---
+++ SpartaTown_LIK/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4f2f93995b5aeef65bd3e8a2cf47e8b07bc5431d44a4a30197b9ae16696b1ef
+size 1050
---
+++ SpartaTown_LIK/Assets/Scripts/UI/PopupStartMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4194a2cb61b4c79dc8c3edb5541a357b97b354a662931296bef9af88869e7815
+size 1249
